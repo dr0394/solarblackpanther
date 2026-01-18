ebc1492317956c3455e086f1822d0dc04b938489
349websites <hallo@349websites.de>
2026-01-18 23:03:27 +0100

    Updated ImageBanner.tsx

--- src/components/ImageBanner.tsx
+++ src/components/ImageBanner.tsx
@@ -7,11 +7,11 @@ export default function ImageBanner({ imageUrl, alt }: ImageBannerProps) {
   return (
     <section className="py-16 md:py-24 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="relative h-[400px] md:h-[500px] lg:h-[600px] w-full overflow-hidden rounded-2xl shadow-2xl">
+        <div className="relative h-[300px] md:h-[500px] lg:h-[600px] w-full overflow-hidden rounded-2xl shadow-2xl bg-gray-100">
           <img
             src={imageUrl}
             alt={alt}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain"
           />
           <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-transparent to-black/10"></div>
         </div>
